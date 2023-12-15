71fc51c2db942a15bd25e8f1900e594a54738d98
davixiao <avidxiao@gmail.com>
2023-12-14 21:31:42 -0500

    solved

---
+++ 13a.cc
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <numeric>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+int horizontal(std::vector<std::string> &grid) {
+    for (auto split = 0; split < grid.size() - 1; ++split) {
+        auto top = split;
+        auto bot = split+1;
+        bool good = true;
+        // std::cout << "split here: " << split << "\n";
+        while (0 <= top && bot < grid.size()) {
+            for (auto i = 0; i < grid[top].size(); ++i) {
+                if (grid[top][i] != grid[bot][i]) {
+                    good = false;
+                    break;
+                }
+            }
+            --top;
+            ++bot;
+        }
+        if (good) return 100 * split + 100;
+    }
+    return 0;
+}
+
+int vertical(std::vector<std::string> &grid) {
+    for (auto split = 0; split < grid[0].size() - 1; ++split) {
+        auto left = split;
+        auto right = split+1;
+        bool good = true;
+        while (0 <= left && right < grid[0].size()) {
+            for (auto i = 0; i < grid.size(); ++i) {
+                if (grid[i][left] != grid[i][right]) {
+                    good = false;
+                    break;
+                }
+            }
+            --left;
+            ++right;
+        }
+        if (good) return split + 1;
+    }
+    return 0;
+}
+
+int main() {
+    std::string line;
+    std::vector<std::string> grid;
+    auto ans = 0;
+    while (std::getline(std::cin, line)) {
+        if (line == "") {
+            ans += horizontal(grid) + vertical(grid);
+            grid.clear();
+        }
+        else {
+            grid.push_back(line);
+        }
+    }
+
+    ans += horizontal(grid) + vertical(grid);
+    std::cout << ans << "\n";
+
+    return 0;
+}
